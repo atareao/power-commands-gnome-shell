33185050fa62a73c6e9aeffbab85fb8e69343125
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2020-07-26 09:27:36 +0200

    v2

--- extension.js
+++ extension.js
@@ -28,7 +28,7 @@ imports.gi.versions.St = "1.0";
 
 const Gtk = imports.gi.Gtk;
 const St = imports.gi.St;
-    
+const GObject = imports.gi.GObject;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 const Util = imports.misc.util;
@@ -38,165 +38,167 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Extension = ExtensionUtils.getCurrentExtension();
 const Convenience = Extension.imports.convenience;
 
-class PowerCommandsButton extends PanelMenu.Button{
-    constructor(){
-        super(St.Align.START);
-        this._settings = Convenience.getSettings();
-        Gtk.IconTheme.get_default().append_search_path(
-            Extension.dir.get_child('icons').get_path());
+let PowerCommandsButton = GObject.registerClass(
+    class PowerCommandsButton extends PanelMenu.Button{
+        _init(){
+            super._init(St.Align.START);
+            this._settings = Convenience.getSettings();
+            Gtk.IconTheme.get_default().append_search_path(
+                Extension.dir.get_child('icons').get_path());
 
-        let box = new St.BoxLayout();
+            let box = new St.BoxLayout();
 
-        let icon = new St.Icon({ icon_name: 'emblem-system',
-                                 style_class: 'system-status-icon' });
-        box.add(icon);
-        this.actor.add_child(box);
+            let icon = new St.Icon({ icon_name: 'emblem-system',
+                                     style_class: 'system-status-icon' });
+            box.add(icon);
+            this.actor.add_child(box);
 
-        let systemActions = SystemActions.getDefault();
+            let systemActions = SystemActions.getDefault();
 
-        this.lineaButtons1 = new PopupMenu.PopupBaseMenuItem({
-            reactive: false
-        });
-        this.menu.addMenuItem(this.lineaButtons1)
-
-        var settings = Convenience.getSettings();
-
-        let screensaver_button = null;
-        let lock_screen_button = null;
-        let switch_user_button = null;
-        let logout_button = null;
-        let shutdown_button = null;
-        let suspend_button = null
-
-        screensaver_button = this._createActionButton('preferences-desktop-screensaver-symbolic', 'Salvapantallas');
-        screensaver_button.set_style_class_name('item');
-        screensaver_button.connect('clicked', ()=>{
-            Util.spawn(['gnome-screensaver-command', '--activate']);
-        });
-        this.lineaButtons1.actor.add_actor(screensaver_button);
-
-        if(systemActions.can_lock_screen){
-            lock_screen_button = this._createActionButton('system-lock-screen-symbolic', 'Bloquear');
-            lock_screen_button.set_style_class_name('item');
-            lock_screen_button.connect('clicked', ()=>{
-                systemActions.activateLockScreen();
+            this.lineaButtons1 = new PopupMenu.PopupBaseMenuItem({
+                reactive: false
             });
-            this.lineaButtons1.actor.add_actor(lock_screen_button);
-        }
+            this.menu.addMenuItem(this.lineaButtons1)
 
-        if (systemActions.can_switch_user){
-            switch_user_button = this._createActionButton('system-switch-user-symbolic', 'Cambiar de usuario');
-            switch_user_button.set_style_class_name('item');
-            switch_user_button.connect('clicked', ()=>{
-                systemActions.activateSwitchUser();
-            });
-            this.lineaButtons1.actor.add_actor(switch_user_button);
-        }
+            var settings = Convenience.getSettings();
 
-        if (systemActions.can_logout){
-            logout_button = this._createActionButton('edit-delete-symbolic', 'Cerrar sessión');
-            logout_button.set_style_class_name('item');
-            logout_button.connect('clicked', ()=>{
-                systemActions.activateLogout();
-            });
-            this.lineaButtons1.actor.add_actor(logout_button);
-        }
-
-
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-
-        this.lineaButtons2 = new PopupMenu.PopupBaseMenuItem({
-            reactive: false
-        });
-        this.menu.addMenuItem(this.lineaButtons2)
-
-        let items = new St.BoxLayout({
-            style_class: 'button-box'
-        });
-        this.lineaButtons2.actor.add_actor(items);
-
-        if (systemActions.can_power_off){
-            shutdown_button = this._createActionButton('system-shutdown-symbolic', 'Apagar');
-            shutdown_button.set_style_class_name('item');
-            shutdown_button.connect('clicked', ()=>{
-                systemActions.activatePowerOff();
-            });
-            items.add_actor(shutdown_button);
-        }
-
-        if (systemActions.can_suspend){
-            suspend_button = this._createActionButton('night-light-symbolic', 'Suspender');
-            suspend_button.set_style_class_name('item');
-            suspend_button.connect('clicked', ()=>{
-                systemActions.activateSuspend();
-            });
-            //this.lineaButtons2.actor.add_actor(item);
-            items.add_actor(suspend_button);
-        }
-        this._settingsC = this._settings.connect("changed", () => {
-            if(screensaver_button != null){
-                if(this._settings.get_boolean('show-screensaver')){
-                    screensaver_button.show();
-                }else{
-                    screensaver_button.hide();
-                }
-            }
-            if(lock_screen_button != null){
-                if(this._settings.get_boolean('show-lock-screen')){
-                    lock_screen_button.show();
-                }else{
-                    lock_screen_button.hide();
-                }
-            }
-            if(switch_user_button != null){
-                if(this._settings.get_boolean('show-switch-user')){
-                    switch_user_button.show();
-                }else{
-                    switch_user_button.hide();
-                }
-            }
-            if(logout_button != null){
-                if(this._settings.get_boolean('show-close-session')){
-                    logout_button.show();
-                }else{
-                    logout_button.hide();
-                }
-            }
-            if(shutdown_button != null){
-                if(this._settings.get_boolean('show-shutdown')){
-                    shutdown_button.show();
-                }else{
-                    shutdown_button.hide();
-                }
-            }
-            if(suspend_button != null){
-                if(this._settings.get_boolean('show-suspend')){
-                    suspend_button.show();
-                }else{
-                    suspend_button.hide();
-                }
-            }
-            /*
             let screensaver_button = null;
             let lock_screen_button = null;
             let switch_user_button = null;
             let logout_button = null;
             let shutdown_button = null;
             let suspend_button = null
-            */
-        });
 
+            screensaver_button = this._createActionButton('preferences-desktop-screensaver-symbolic', 'Salvapantallas');
+            screensaver_button.set_style_class_name('item');
+            screensaver_button.connect('clicked', ()=>{
+                Util.spawn(['gnome-screensaver-command', '--activate']);
+            });
+            this.lineaButtons1.actor.add_actor(screensaver_button);
+
+            if(systemActions.can_lock_screen){
+                lock_screen_button = this._createActionButton('system-lock-screen-symbolic', 'Bloquear');
+                lock_screen_button.set_style_class_name('item');
+                lock_screen_button.connect('clicked', ()=>{
+                    systemActions.activateLockScreen();
+                });
+                this.lineaButtons1.actor.add_actor(lock_screen_button);
+            }
+
+            if (systemActions.can_switch_user){
+                switch_user_button = this._createActionButton('system-switch-user-symbolic', 'Cambiar de usuario');
+                switch_user_button.set_style_class_name('item');
+                switch_user_button.connect('clicked', ()=>{
+                    systemActions.activateSwitchUser();
+                });
+                this.lineaButtons1.actor.add_actor(switch_user_button);
+            }
+
+            if (systemActions.can_logout){
+                logout_button = this._createActionButton('edit-delete-symbolic', 'Cerrar sessión');
+                logout_button.set_style_class_name('item');
+                logout_button.connect('clicked', ()=>{
+                    systemActions.activateLogout();
+                });
+                this.lineaButtons1.actor.add_actor(logout_button);
+            }
+
+
+            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+            this.lineaButtons2 = new PopupMenu.PopupBaseMenuItem({
+                reactive: false
+            });
+            this.menu.addMenuItem(this.lineaButtons2)
+
+            let items = new St.BoxLayout({
+                style_class: 'button-box'
+            });
+            this.lineaButtons2.actor.add_actor(items);
+
+            if (systemActions.can_power_off){
+                shutdown_button = this._createActionButton('system-shutdown-symbolic', 'Apagar');
+                shutdown_button.set_style_class_name('item');
+                shutdown_button.connect('clicked', ()=>{
+                    systemActions.activatePowerOff();
+                });
+                items.add_actor(shutdown_button);
+            }
+
+            if (systemActions.can_suspend){
+                suspend_button = this._createActionButton('night-light-symbolic', 'Suspender');
+                suspend_button.set_style_class_name('item');
+                suspend_button.connect('clicked', ()=>{
+                    systemActions.activateSuspend();
+                });
+                //this.lineaButtons2.actor.add_actor(item);
+                items.add_actor(suspend_button);
+            }
+            this._settingsC = this._settings.connect("changed", () => {
+                if(screensaver_button != null){
+                    if(this._settings.get_boolean('show-screensaver')){
+                        screensaver_button.show();
+                    }else{
+                        screensaver_button.hide();
+                    }
+                }
+                if(lock_screen_button != null){
+                    if(this._settings.get_boolean('show-lock-screen')){
+                        lock_screen_button.show();
+                    }else{
+                        lock_screen_button.hide();
+                    }
+                }
+                if(switch_user_button != null){
+                    if(this._settings.get_boolean('show-switch-user')){
+                        switch_user_button.show();
+                    }else{
+                        switch_user_button.hide();
+                    }
+                }
+                if(logout_button != null){
+                    if(this._settings.get_boolean('show-close-session')){
+                        logout_button.show();
+                    }else{
+                        logout_button.hide();
+                    }
+                }
+                if(shutdown_button != null){
+                    if(this._settings.get_boolean('show-shutdown')){
+                        shutdown_button.show();
+                    }else{
+                        shutdown_button.hide();
+                    }
+                }
+                if(suspend_button != null){
+                    if(this._settings.get_boolean('show-suspend')){
+                        suspend_button.show();
+                    }else{
+                        suspend_button.hide();
+                    }
+                }
+                /*
+                let screensaver_button = null;
+                let lock_screen_button = null;
+                let switch_user_button = null;
+                let logout_button = null;
+                let shutdown_button = null;
+                let suspend_button = null
+                */
+            });
+
+        }
+        _createActionButton(iconName, accessibleName) {
+            let icon = new St.Button({ reactive: true,
+                                       can_focus: true,
+                                       track_hover: true,
+                                       accessible_name: accessibleName,
+                                       style_class: 'system-menu-action' });
+            icon.child = new St.Icon({ icon_name: iconName });
+            return icon;
+        }
     }
-    _createActionButton(iconName, accessibleName) {
-        let icon = new St.Button({ reactive: true,
-                                   can_focus: true,
-                                   track_hover: true,
-                                   accessible_name: accessibleName,
-                                   style_class: 'system-menu-action' });
-        icon.child = new St.Icon({ icon_name: iconName });
-        return icon;
-    }
-}
+);
 
 let powerCommandsButton;
 
--- prefs.js
+++ prefs.js
@@ -18,102 +18,106 @@ function init() {
     Convenience.initTranslations();
 }
 
-class AboutWidget extends Gtk.Grid{
-    constructor() {
-        super({
-            margin_bottom: 18,
-            row_spacing: 8,
-            hexpand: true,
-            halign: Gtk.Align.CENTER,
-            orientation: Gtk.Orientation.VERTICAL
-        });
+var AboutWidget = GObject.registerClass(
+    class AboutWidget extends Gtk.Grid{
+        _init() {
+            super._init({
+                margin_bottom: 18,
+                row_spacing: 8,
+                hexpand: true,
+                halign: Gtk.Align.CENTER,
+                orientation: Gtk.Orientation.VERTICAL
+            });
 
-        let aboutIcon = new Gtk.Image({
-            icon_name: "pc-shutdown",
-            pixel_size: 128
-        });
-        this.add(aboutIcon);
+            let aboutIcon = new Gtk.Image({
+                icon_name: "pc-shutdown",
+                pixel_size: 128
+            });
+            this.add(aboutIcon);
 
-        let aboutName = new Gtk.Label({
-            label: "<b>" + _("Power Commands") + "</b>",
-            use_markup: true
-        });
-        this.add(aboutName);
+            let aboutName = new Gtk.Label({
+                label: "<b>" + _("Power Commands") + "</b>",
+                use_markup: true
+            });
+            this.add(aboutName);
 
-        let aboutVersion = new Gtk.Label({ label: _('Version: ') + Extension.metadata.version.toString() });
-        this.add(aboutVersion);
+            let aboutVersion = new Gtk.Label({ label: _('Version: ') + Extension.metadata.version.toString() });
+            this.add(aboutVersion);
 
-        let aboutDescription = new Gtk.Label({
-            label:  Extension.metadata.description
-        });
-        this.add(aboutDescription);
+            let aboutDescription = new Gtk.Label({
+                label:  Extension.metadata.description
+            });
+            this.add(aboutDescription);
 
-        let aboutWebsite = new Gtk.Label({
-            label: '<a href="%s">%s</a>'.format(
-                Extension.metadata.url,
-                _("Atareao")
-            ),
-            use_markup: true
-        });
-        this.add(aboutWebsite);
+            let aboutWebsite = new Gtk.Label({
+                label: '<a href="%s">%s</a>'.format(
+                    Extension.metadata.url,
+                    _("Atareao")
+                ),
+                use_markup: true
+            });
+            this.add(aboutWebsite);
 
-        let aboutCopyright = new Gtk.Label({
-            label: "<small>" + _('Copyright © 2018 Lorenzo Carbonell') + "</small>",
-            use_markup: true
-        });
-        this.add(aboutCopyright);
+            let aboutCopyright = new Gtk.Label({
+                label: "<small>" + _('Copyright © 2018 Lorenzo Carbonell') + "</small>",
+                use_markup: true
+            });
+            this.add(aboutCopyright);
 
-        let aboutLicense = new Gtk.Label({
-            label: "<small>" +
-            _("This program is free software: you can redistribute it and/or modify") + "\n" +
-            _("it under the terms of the GNU General Public License as published by") + "\n" +
-            _("the Free Software Foundation, either version 3 of the License, or") + "\n" +
-            _("(at your option) any later version.") + "\n\n" +
-            _("This program is distributed in the hope that it will be useful,") + "\n" +
-            _("but WITHOUT ANY WARRANTY; without even the implied warranty of") + "\n" +
-            _("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the") + "\n" +
-            _("GNU General Public License for more details.") + "\n\n" +
-            _("You should have received a copy of the GNU General Public License") + "\n" +
-            _("along with this program.  If not, see <a href=\"https://www.gnu.org/licenses/\">https://www.gnu.org/licenses/</a>.") + "\n" +
-            "</small>",
-            use_markup: true,
-            justify: Gtk.Justification.CENTER
-        });
-        this.add(aboutLicense);
+            let aboutLicense = new Gtk.Label({
+                label: "<small>" +
+                _("This program is free software: you can redistribute it and/or modify") + "\n" +
+                _("it under the terms of the GNU General Public License as published by") + "\n" +
+                _("the Free Software Foundation, either version 3 of the License, or") + "\n" +
+                _("(at your option) any later version.") + "\n\n" +
+                _("This program is distributed in the hope that it will be useful,") + "\n" +
+                _("but WITHOUT ANY WARRANTY; without even the implied warranty of") + "\n" +
+                _("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the") + "\n" +
+                _("GNU General Public License for more details.") + "\n\n" +
+                _("You should have received a copy of the GNU General Public License") + "\n" +
+                _("along with this program.  If not, see <a href=\"https://www.gnu.org/licenses/\">https://www.gnu.org/licenses/</a>.") + "\n" +
+                "</small>",
+                use_markup: true,
+                justify: Gtk.Justification.CENTER
+            });
+            this.add(aboutLicense);
+        }
     }
-}
+);
 
-class PowerCommandsPreferencesWidget extends PreferencesWidget.Stack{
-    constructor(){
-        super();
+var PowerCommandsPreferencesWidget = GObject.registerClass(
+    class PowerCommandsPreferencesWidget extends PreferencesWidget.Stack{
+        _init(){
+            super._init();
 
-        // Preferences Page
-        let preferencesPage = this.addPage(
-            "preferences",
-            _("Preferences"),
-            {}
-        );
+            // Preferences Page
+            let preferencesPage = this.addPage(
+                "preferences",
+                _("Preferences"),
+                {}
+            );
 
-        var settings = Convenience.getSettings();
-        
-        let appearanceSection = preferencesPage.addSection(_("Select buttons"), null, {});
-        appearanceSection.addGSetting(settings, "show-screensaver", undefined);
-        appearanceSection.addGSetting(settings, "show-lock-screen");
-        appearanceSection.addGSetting(settings, "show-switch-user");
-        appearanceSection.addGSetting(settings, "show-close-session");
-        appearanceSection.addGSetting(settings, "show-shutdown");
-        appearanceSection.addGSetting(settings, "show-suspend");
+            var settings = Convenience.getSettings();
+            
+            let appearanceSection = preferencesPage.addSection(_("Select buttons"), null, {});
+            appearanceSection.addGSetting(settings, "show-screensaver", undefined);
+            appearanceSection.addGSetting(settings, "show-lock-screen");
+            appearanceSection.addGSetting(settings, "show-switch-user");
+            appearanceSection.addGSetting(settings, "show-close-session");
+            appearanceSection.addGSetting(settings, "show-shutdown");
+            appearanceSection.addGSetting(settings, "show-suspend");
 
-        // About Page
-        let aboutPage = this.addPage(
-            "about",
-            _("About"),
-            { vscrollbar_policy: Gtk.PolicyType.NEVER }
-        );
-        aboutPage.box.add(new AboutWidget());
-        aboutPage.box.margin_top = 18;
+            // About Page
+            let aboutPage = this.addPage(
+                "about",
+                _("About"),
+                { vscrollbar_policy: Gtk.PolicyType.NEVER }
+            );
+            aboutPage.box.add(new AboutWidget());
+            aboutPage.box.margin_top = 18;
+        }
     }
-}
+);
 
 function buildPrefsWidget() {
     let tipw = new PowerCommandsPreferencesWidget();
